f923ea80e0b964d4898e8e079fd79e851981768e
nilayupadhyay17 <nilay175@gmail.com>
2025-03-12 12:02:53 -0400

    Initial Commit

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+import pytest
+
+from myapp.app import multiply_by_two, divide_by_two, calculate_area
+
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    return [a,b]
+
+
+class TestApp:
+    def test_multiplication(self, numbers):
+        res = multiply_by_two(numbers[0])
+        assert res == numbers[1]
+
+    def test_division(self, numbers):
+        res = divide_by_two(numbers[1])
+        assert res == numbers[0]
+    
+    def test_area_last_digits(self):
+        assert calculate_area(43) == 184
